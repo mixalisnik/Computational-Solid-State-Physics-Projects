302be3fec1fc1fd123d6f0f9cb796ad6c1fa5007
mixalisnik <101192432+mixalisnik@users.noreply.github.com>
2022-03-28 18:49:48 +0300

    Add files via upload

---
+++ 1D-Random-Walk-R-Squared-Steps-Figure.py
@@ -0,0 +1,37 @@
+import numpy as np
+from matplotlib import pyplot as plt 
+
+
+#1 dimension Random Walk
+n=10000
+steps=1000
+grid_num=1000
+pos_x=np.random.randint(0,grid_num)
+x_points=np.linspace(0,steps,int(steps/100)+1)
+delta_x=np.zeros([int(steps/100)+1,1])
+r_squared=np.zeros([int(steps/100)+1,1])
+nt=np.zeros([int(steps/100)+1,1])
+for j in range(n):
+    pos_xo=pos_x
+    for i in range(steps+1):
+        if pos_x!=0 and pos_x!=grid_num:
+            pos_x=pos_x+np.random.choice([-1,1])
+        elif pos_x==0:
+            pos_x=pos_x+1
+        elif pos_x==grid_num:
+            pos_x=pos_x-1
+        if i%100==0:
+            delta_x[int(i/100)]=pos_x-pos_xo
+            r_squared[int(i/100)]=r_squared[int(i/100)]+delta_x[int(i/100)]**2
+            nt[int(i/100)]=nt[int(i/100)]+1
+plt.figure()
+plt.title('R squared average - steps')
+plt.xlabel('steps')
+plt.ylabel('R squared average')
+plt.scatter(x_points,r_squared/n)     
+a,b=np.polyfit(x_points,r_squared/n,1)
+x_row=np.linspace(0,steps,1000)
+y=a*x_row+b
+plt.plot(x_row,y,c='red')
+plt.grid()
+
